26fd132b6424c9ce97068b6a775ff5b194a327f9
Gonçalo Silva <goncalossilva@gmail.com>
2014-07-28 19:17:52 +0100

    Implement getCenter() in LatLngBounds

--- GoogleMapsEverywhere/src/main/java/com/goncalossilva/googlemapseverywhere/model/LatLngBounds.java
+++ GoogleMapsEverywhere/src/main/java/com/goncalossilva/googlemapseverywhere/model/LatLngBounds.java
@@ -4,8 +4,20 @@ public class LatLngBounds {
     public final LatLng northeast;
     public final LatLng southwest;
 
-    public LatLngBounds(LatLng northeast, LatLng southwest) {
-        this.northeast = northeast;
+    public LatLngBounds(LatLng southwest, LatLng northeast) {
         this.southwest = southwest;
+        this.northeast = northeast;
+    }
+
+    public LatLng getCenter() {
+        double latitudeCenter = (southwest.latitude + northeast.latitude) / 2D;
+        double longitudeCenter;
+        if (southwest.longitude <= northeast.longitude) {
+            longitudeCenter = (northeast.longitude + southwest.longitude) / 2D;
+        }
+        else {
+            longitudeCenter = (northeast.longitude + 360D + southwest.longitude) / 2D;
+        }
+        return new LatLng(latitudeCenter, longitudeCenter);
     }
 }
